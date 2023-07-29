74b85c0c49869a90fc74f6fd894faa12188bbb7c
fengjutian <2640111081@qq.com>
2023-07-29 20:13:04 +0800

    save

--- nanjing-shop-serve/src/main/java/com/fengjutian/demo/controller/HelloController.java
+++ nanjing-shop-serve/src/main/java/com/fengjutian/demo/controller/HelloController.java
@@ -1,10 +1,13 @@
+package com.fengjutian.demo.controller;
 
+import org.springframework.stereotype.Controller;
 
 @Controller
-public clsdd HelloController(){
-@GetMapping("demo")
-    public String demo(Model model){
-        model.addAttribute("title","SpringBoot中集成使用jsp");
+public class HelloController(){
+
+    @GetMapping("demo")
+    public String demo(){
+
         return "demo";
     }
 
