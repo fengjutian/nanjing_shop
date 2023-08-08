a9a30fa73bd8e7dd275cd63745ef83b387b2acf3
fengjutian <2640111081@qq.com>
2023-08-08 22:42:48 +0800

    save

--- nanjing-shop-client/src/app/page.tsx
+++ nanjing-shop-client/src/app/page.tsx
@@ -6,9 +6,7 @@ import { Amap, Marker } from '@amap/amap-react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Map amapkey='3f46074b02e9ae77c705e5280dd771d6' version='v5' ></Map> */}
-
+    <div style={{ width: '100%', height: '400px' }}>
       <Amap
         mapStyle="amap://styles/whitesmoke"
         zoom={15}
@@ -22,7 +20,29 @@ export default function Home() {
           }}
         />
       </Amap>
- 
-    </main>
-  )
+    </div>
+  );
 }
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       {/* <Map amapkey='3f46074b02e9ae77c705e5280dd771d6' version='v5' ></Map> */}
+
+//       <Amap
+//         mapStyle="amap://styles/whitesmoke"
+//         zoom={15}
+//         center={[116.473179, 39.993169]}
+//       >
+//         <Marker
+//           position={[116.473179, 39.993169]}
+//           label={{
+//             content: 'Hello, AMap-React!',
+//             direction: 'bottom',
+//           }}
+//         />
+//       </Amap>
+ 
+//     </main>
+//   )
+// }
