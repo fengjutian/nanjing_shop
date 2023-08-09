1ee9c744bd9561c34bf6526dda0447abf284e3f1
fengjutian <2640111081@qq.com>
2023-08-09 21:09:09 +0800

    优化

--- nanjing-shop-client/src/app/page.tsx
+++ nanjing-shop-client/src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import Image from 'next/image'
+import { useState } from 'react';
 import { Amap, Marker, config as AmapReactConfig } from '@amap/amap-react';
-import { Button, Input } from "@arco-design/web-react";
+import { Button, Input, Radio } from "@arco-design/web-react";
+import './page.css'
+const RadioGroup = Radio.Group;
 const InputSearch = Input.Search;
 
 AmapReactConfig.version = '2.0'; // 默认2.0，这里可以不修改
@@ -12,17 +15,79 @@ AmapReactConfig.plugins = [
 ];
 
 export default function Home() {
+
+  let mapStyle = useState('normal')
+
+  const mapStyleList = [
+    {
+      title: '标准',
+      type: 'normal'
+    },
+    {
+      title: '马卡龙',
+      type: 'macaron'
+    },
+    {
+      title: '涂鸦',
+      type: 'graffiti'
+    },
+    {
+      title: '远山黛',
+      type: 'whitesmoke'
+    },
+    {
+      title: '幻影黑',
+      type: 'dark'
+    },
+    {
+      title: '草色青',
+      type: 'fresh'
+    },
+    {
+      title: '极夜蓝',
+      type: 'darkblue'
+    },
+    {
+      title: '靛青蓝',
+      type: 'blue'
+    },
+    {
+      title: '月光银',
+      type: 'light'
+    },
+    {
+      title: '雅士灰',
+      type: 'grey'
+    }
+  ]
+
+  // mapList(){
+
+  // }
+
+
   return (
     <div style={{ width: '100%', height: '100vh' }}>
+      <div className='map-style-wrap'>
+        <RadioGroup direction='vertical' defaultValue='a'>
+          {
+            mapStyleList.map((item, index) => {
+              return(
+                <Radio value='a'>{item.title}</Radio>
+              )
+            })
+          }
+        </RadioGroup>
+      </div>
+
       <InputSearch
         searchButton='Search'
-        
         defaultValue='Search content'
         placeholder='Enter keyword to search'
         style={{ width: 350 }}
       />
       <Amap
-        mapStyle="amap://styles/normal"
+        mapStyle={'amap://styles/' + mapStyle}
         zoom={15}
         center={[118.784251,32.041918]}
       >
